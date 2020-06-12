6a90029d52e8c167d4e1af08c332f0d6d9e51896
Litzy Mariana <17030609@itcelaya.edu.mx>
2020-06-12 18:47:18 -0500

    Conexion

--- src/sample/conexion.java
+++ src/sample/conexion.java
@@ -7,8 +7,8 @@ public class conexion {
 
     //volvemos constantes las bariables con final
     private static final String host = "localhost";
-    private static final String  user = "topicos20";
-    private static final String pwd = "Conectar123";
+    private static final String  user = "root";
+    private static final String pwd = "Topicos123";
     private static final String db = "taqueria";
 
     public static Connection con;
@@ -18,6 +18,7 @@ public class conexion {
             Class.forName("com.mysql.jdbc.Driver");
             //se abre el socket hacia el SGBD
             con = DriverManager.getConnection("jdbc:mysql://"+host+":3306/"+db+"?useSSL=false",user,pwd);
+            System.out.println("se ha iniciado la conexion");
         } catch (Exception e ) {
             e.printStackTrace();
         }
